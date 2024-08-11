d9c2de946db1592ce226d1c694871bbefdbd8b86
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-08-11 17:06:46 -0500

    agregamos compatibilidad con react y tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	devToolbar: {enabled : false},
+  devToolbar: {
+    enabled: false
+  },
+  integrations: [react(), tailwind()]
 });
