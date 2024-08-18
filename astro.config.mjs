ca0ab681404ae0c3df4033a3fd0e7b5c22962e98
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-08-18 16:33:52 -0500

    se agrego el adaptador para Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,6 @@ export default defineConfig({
   output: "server",
   redirects: {
     '/': '/home'
-  }
+  },
+  adapter: vercel()
 });
