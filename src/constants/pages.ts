bfb7cf0bf81a52559030df7cb19cccbe75adfb9a
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-08-18 16:20:48 -0500

    Creamos Constantes para las distintas rutas y adaptamos la pagina web para utilizarlas

--- src/constants/pages.ts
+++ src/constants/pages.ts
@@ -1,5 +1,38 @@
-export enum CurrentPage {
+export enum CurrentLandingPage {
 	Home,
 	About,
 	HowItWorks,
 }
+
+export interface Page {
+	title: string
+	url: string
+}
+
+export const LandingPages = {
+	Home: {
+		title: 'Home',
+		url: '/home',
+	},
+	About: {
+		title: 'About',
+		url: '/about',
+	},
+	HowItWorks: {
+		title: 'How It Works',
+		url: '/how-it-works',
+	},
+}
+
+export const AuthPages = {
+	SignIn: {
+		title: 'Sign In',
+		url: '/sign-in',
+	},
+	SignUp: {
+		title: 'Sign Up',
+		url: '/sign-up',
+	},
+}
+
+export const AppPages = {}
