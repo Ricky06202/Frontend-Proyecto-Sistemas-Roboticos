3687551a671cf32029788b66837e81e3aa59d54b
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-09-02 13:58:52 -0500

    hicimos responsive el menu de opciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
-    enabled: false
+    enabled: false,
   },
   integrations: [react(), tailwind()],
   output: "server",
   redirects: {
-    '/': '/home'
+    "/": "/home",
   },
-  adapter: vercel()
+  adapter: vercel(),
 });
