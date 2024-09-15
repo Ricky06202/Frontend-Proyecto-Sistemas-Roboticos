b5040a1f27e85f497af2ea59d34e79fff9a0e9e3
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-09-14 19:55:12 -0500

    probando visibilidad de la imagen

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig, passthroughImageService } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  devToolbar: {
-    enabled: false,
-  },
-  integrations: [react(), tailwind()],
-  output: "server",
-  redirects: {
-    "/": "/home",
-  },
-  adapter: vercel(),
-});
+	devToolbar: {
+		enabled: false,
+	},
+	integrations: [react(), tailwind()],
+	output: 'server',
+	redirects: {
+		'/': '/home',
+	},
+	adapter: vercel(),
+	image: {
+		service: passthroughImageService(),
+	},
+})
