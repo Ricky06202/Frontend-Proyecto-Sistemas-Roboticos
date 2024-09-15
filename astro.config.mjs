b71983ebe565c8ebe0ea295cbdeb185533c5457b
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-09-14 19:49:57 -0500

    Revert "probando la configuracion para las assets"
    This reverts commit 6b0faad092a6268b46a7a2d0cbad0585fba14ad9.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,17 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	devToolbar: {
-		enabled: false,
-	},
-	integrations: [react(), tailwind()],
-	output: 'server',
-	redirects: {
-		'/': '/home',
-	},
-	adapter: vercel(),
-	build: {
-		assets: ['src/components/**/*', 'src/layouts/**/*', 'src/modules/**/*'],
-	},
-})
+  devToolbar: {
+    enabled: false,
+  },
+  integrations: [react(), tailwind()],
+  output: "server",
+  redirects: {
+    "/": "/home",
+  },
+  adapter: vercel(),
+});
