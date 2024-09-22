658742d64745d488bf13b896e706c9cc4fc01a59
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-09-22 12:58:37 -0500

    Personalizamos la asistencia Simple

--- src/modules/attendance/components/student/AsistenciaSimple.tsx
+++ src/modules/attendance/components/student/AsistenciaSimple.tsx
@@ -2,19 +2,25 @@ import { Icon } from '@iconify/react'
 export default function AsistenciaSimple() {
 	return (
 		<section className='flex flex-col gap-4'>
-			<header className='flex flex-col gap-4 items-center'>
-				<div>
-					<button className='focus:outline-none'>
-						<Icon icon='mingcute:left-fill' />
+			<header className='flex flex-col items-center gap-4'>
+				<div className='flex gap-4'>
+					<button className='focus:outline-none hover:text-blue-500'>
+						<Icon
+							icon='mingcute:left-fill'
+							className='text-3xl'
+						/>
 					</button>
-					<select className='focus:outline-none'>
+					<select className='px-4 py-2 border-2 rounded-lg outline-none border-blue-50 focus:border-blue-300 bg-blue-50'>
 						<option value='1'>Semana 1</option>
 						<option value='2'>Semana 2</option>
 						<option value='3'>Semana 3</option>
 						<option value='4'>Semana 4</option>
 					</select>
-					<button className='focus:outline-none'>
-						<Icon icon='mingcute:right-fill' />
+					<button className='focus:outline-none hover:text-blue-500'>
+						<Icon
+							icon='mingcute:right-fill'
+							className='text-3xl'
+						/>
 					</button>
 				</div>
 			</header>
@@ -24,7 +30,7 @@ export default function AsistenciaSimple() {
 				<table>
 					<thead>
 						<tr>
-							<th className='text-left py-4'>Curso</th>
+							<th className='py-4 text-left'>Curso</th>
 							<th className='border-l'>Lunes</th>
 							<th className='border-l'>Martes</th>
 							<th className='border-l'>Miércoles</th>
@@ -33,36 +39,41 @@ export default function AsistenciaSimple() {
 						</tr>
 					</thead>
 					<tbody>
-						<tr className='border-t text-2xl'>
-							<td className='text-left py-6 text-lg'>Calculo 1</td>
+						<tr className='text-2xl text-left border-t'>
+							<td className='py-6 text-lg text-left'>Calculo 1</td>
 							<td className='border-l'>
 								<Icon
 									icon='mingcute:check-2-fill'
-									className='text-green-600 mx-auto'
+									mx-a
+									className='mx-auto text-green-600'
 								/>
 							</td>
 							<td className='border-l'>
 								<Icon
 									icon='mingcute:close-fill'
-									className='text-red-600 mx-auto'
+									mx-a
+									className='mx-auto text-red-600'
 								/>
 							</td>
 							<td className='border-l'>
 								<Icon
 									icon='mingcute:check-2-fill'
-									className='text-green-600 mx-auto'
+									mx-a
+									className='mx-auto text-green-600'
 								/>
 							</td>
 							<td className='border-l'>
 								<Icon
 									icon='mingcute:check-2-fill'
-									className='text-green-600 mx-auto'
+									mx-a
+									className='mx-auto text-green-600'
 								/>
 							</td>
 							<td className='border-l'>
 								<Icon
 									icon='mingcute:close-fill'
-									className='text-red-600 mx-auto'
+									mx-a
+									className='mx-auto text-red-600'
 								/>
 							</td>
 						</tr>
