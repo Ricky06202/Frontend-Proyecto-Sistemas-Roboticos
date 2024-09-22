3d960b69abeef0731624d4f5c00a114d7dea5cc5
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-09-22 11:59:38 -0500

    corregimos un error de estilos

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,8 @@ export default function Button({ text, class: className, passThrough }: Props) {
 				<a
 					href={AppPages.Student.Attendance.url}
 					className={
-						'relative px-6 py-3 overflow-hidden font-bold text-white bg-blue-700 rounded-lg group ' + className
+						'relative px-6 py-3 overflow-hidden font-bold text-white bg-blue-700 rounded-lg group text-center ' +
+						className
 					}
 				>
 					<span className='absolute top-0 left-0 w-full h-0 transition-all duration-300 ease-in-out bg-black/10 group-hover:h-full group-hover:bottom-0 group-hover:top-auto'></span>
