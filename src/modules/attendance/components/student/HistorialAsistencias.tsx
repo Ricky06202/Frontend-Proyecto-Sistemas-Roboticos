65ae526c5c5709646bc3cbe900113639f14ebe8c
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-10-05 14:05:27 -0500

    lo configuramos para que funcione con los datos mockeados

--- src/modules/attendance/components/student/HistorialAsistencias.tsx
+++ src/modules/attendance/components/student/HistorialAsistencias.tsx
@@ -1,12 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import AsistenciaDetallada from './AsistenciaDetallada'
 import AsistenciaSimple from './AsistenciaSimple'
+import { getEstudiante } from '@attendance/services/apiHorario'
+import type { Asistencia, Estudiante } from '@attendance/constants/apiTypes'
+import { getAsistencias } from '@attendance/services/apiAsistencia'
+import { getEstudiantesMock } from '@attendance/constants/horarioMock'
+import { getAsistenciasMock } from '@attendance/constants/asistenciaMock'
+import { useAttendanceStore } from '@attendance/store/attendanceStore'
 
-export default function HistorialAsistencias() {
+interface Props {
+	id: number
+}
+
+export default function HistorialAsistencias({ id }: Props) {
 	const [isSimpleAttendanceView, toggleAttendanceView] = useState(true)
 	function handleToggleAttendanceView() {
 		toggleAttendanceView(!isSimpleAttendanceView)
 	}
+
+	const [estudiante, setEstudiante] = useState<Estudiante | null>(null)
+	const [asistencias, setAsistencias] = useAttendanceStore((state) => [state.attendance, state.setAttendance])
+
+	useEffect(() => {
+		// 	async function getInfo() {
+		// 		const dataEstudiante = await getEstudiante(id)
+		// 		setEstudiante(dataEstudiante)
+		// 		const dataAsistencias = await getAsistencias()
+		// 		setAsistencias(dataAsistencias)
+		// 	}
+		function getInfo() {
+			setEstudiante(getEstudiantesMock().filter((estudiante) => estudiante.id === id)[0])
+			setAsistencias(getAsistenciasMock())
+		}
+
+		getInfo()
+	}, [])
+
 	return (
 		<>
 			<div className='flex flex-col items-center justify-center gap-4 lg:flex-row'>
@@ -18,7 +47,15 @@ export default function HistorialAsistencias() {
 					{isSimpleAttendanceView ? 'Simple' : 'Detallada'}
 				</button>
 			</div>
-			{isSimpleAttendanceView ? <AsistenciaSimple /> : <AsistenciaDetallada />}
+			{estudiante && asistencias ? (
+				isSimpleAttendanceView ? (
+					<AsistenciaSimple />
+				) : (
+					<AsistenciaDetallada />
+				)
+			) : (
+				<div className='text-center w-full text-xl  italic'>Loading...</div>
+			)}
 		</>
 	)
 }
