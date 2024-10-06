75baf08cb35206b3604e92ae5bf9228bdc6e3bda
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-10-05 20:50:25 -0500

    se integro la api para desplegar las materias y las asistencias del alumno

--- src/modules/attendance/components/student/AsistenciaDetallada.tsx
+++ src/modules/attendance/components/student/AsistenciaDetallada.tsx
@@ -1,8 +1,13 @@
+import type { Estudiante } from '@attendance/constants/apiTypes'
+import type { Materia } from '@attendance/constants/materiasTypes'
 import { Icon } from '@iconify/react'
-export default function AsistenciaDetallada() {
+interface Props {
+	materias: Materia[]
+}
+export default function AsistenciaDetallada({ materias }: Props) {
 	return (
-		<section className='flex flex-col gap-4'>
-			<div className='flex justify-center gap-4'>
+		<section className='flex flex-col gap-4 overflow-x-hidden'>
+			<header className='flex justify-center gap-4'>
 				<select className='px-4 py-2 border-2 rounded-lg outline-none border-blue-50 focus:border-blue-300 bg-blue-50'>
 					<option
 						value='0'
@@ -28,37 +33,51 @@ export default function AsistenciaDetallada() {
 					<option value='2'>Asistencias</option>
 					<option value='3'>Inasistencias</option>
 				</select>
-			</div>
+			</header>
 			{/* este div es una linea divisora */}
 			<div className='w-full h-px bg-gray-300'></div>
-			<div className='flex flex-col p-4 border-2 rounded-lg'>
-				<table className=''>
-					<thead>
-						<tr>
-							<th className='py-4 text-left'>Curso</th>
-							<th className='border-l'>Horario</th>
-							<th className='border-l'>Registro</th>
-							<th className='border-l'>Asistencia</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr className='border-t'>
-							<td className='py-6 text-lg'>Calculo 1</td>
-							<td className='text-center border-l'>
-								Lunes<div>10:00 - 11:00</div>
-							</td>
-							<td className='text-center border-l'>
-								Jun 10, 2024 <div>10:00 AM</div>
-							</td>
-							<td className='border-l'>
-								<Icon
-									icon='mingcute:check-2-fill'
-									className='mx-auto text-2xl text-green-600'
-								/>
-							</td>
-						</tr>
-					</tbody>
-				</table>
+			{/*className='overflow-y-auto max-h-[1000px]' esto es por si acaso pensamos en hacerlo como una area delimitada*/}
+			<div className='flex flex-col gap-4'>
+				{materias.map((materia) => (
+					<div className='flex flex-col p-4 border-2 rounded-lg overflow-x-auto '>
+						<table className=''>
+							<thead>
+								<tr>
+									<th className='py-4 text-left min-w-72'>Curso</th>
+									<th className='border-l min-w-36'>Horario</th>
+									<th className='border-l min-w-36'>Registro</th>
+									<th className='border-l min-w-36'>Asistencia</th>
+								</tr>
+							</thead>
+							<tbody>
+								{materia.dias.map((dia) => (
+									<tr className='border-t'>
+										<td className='py-6 text-lg'>{materia.nombre}</td>
+										<td className='text-center border-l'>
+											{dia.nombre} <div>{`${dia.inicio.slice(0, -3)} - ${dia.fin.slice(0, -3)}`}</div>
+										</td>
+										<td className='text-center border-l'>
+											{dia.asistencia.fecha} <div>{dia.asistencia.hora.slice(0, -3)}</div>
+										</td>
+										<td className='border-l'>
+											{dia.asistencia.valor ? (
+												<Icon
+													icon='mingcute:check-2-fill'
+													className='mx-auto text-2xl text-green-600'
+												/>
+											) : (
+												<Icon
+													icon='mingcute:close-fill'
+													className='mx-auto text-2xl text-red-600'
+												/>
+											)}
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
+				))}
 			</div>
 		</section>
 	)
