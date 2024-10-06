42eb877c4d97b63bcbe3301d2775d48e0c6bacfb
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-10-05 20:50:00 -0500

    configuramos el historial para usar los datos mockeados

--- src/modules/attendance/components/student/HistorialAsistencias.tsx
+++ src/modules/attendance/components/student/HistorialAsistencias.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
 import AsistenciaDetallada from './AsistenciaDetallada'
 import AsistenciaSimple from './AsistenciaSimple'
-import { getEstudiante } from '@attendance/services/apiHorario'
-import type { Asistencia, Estudiante } from '@attendance/constants/apiTypes'
-import { getAsistencias } from '@attendance/services/apiAsistencia'
+import type { Estudiante } from '@attendance/constants/apiTypes'
 import { getEstudiantesMock } from '@attendance/constants/horarioMock'
-import { getAsistenciasMock } from '@attendance/constants/asistenciaMock'
 import { useAttendanceStore } from '@attendance/store/attendanceStore'
+import { useStudentsStore } from '@attendance/store/studentsStore'
+import { getAsistenciasMock } from '@attendance/constants/asistenciaMock'
+import { ordenarMaterias } from '@attendance/logic/ordenarMaterias'
 
 interface Props {
 	id: number
@@ -18,28 +18,25 @@ export default function HistorialAsistencias({ id }: Props) {
 		toggleAttendanceView(!isSimpleAttendanceView)
 	}
 
-	const [estudiante, setEstudiante] = useState<Estudiante | null>(null)
-	const [asistencias, setAsistencias] = useAttendanceStore((state) => [state.attendance, state.setAttendance])
+	// const [estudiantes, fetchEstudiantes] = useStudentsStore((state) => [state.students, state.fetchStudents])
+	// const [asistencias, fetchAsistencias] = useAttendanceStore((state) => [state.attendance, state.fetchAttendance])
+	const estudiantes = getEstudiantesMock()
+	const asistencias = getAsistenciasMock()
 
-	useEffect(() => {
-		// 	async function getInfo() {
-		// 		const dataEstudiante = await getEstudiante(id)
-		// 		setEstudiante(dataEstudiante)
-		// 		const dataAsistencias = await getAsistencias()
-		// 		setAsistencias(dataAsistencias)
-		// 	}
-		function getInfo() {
-			setEstudiante(getEstudiantesMock().filter((estudiante) => estudiante.id === id)[0])
-			setAsistencias(getAsistenciasMock())
-		}
-
-		getInfo()
-	}, [])
-
+	const estudiante = estudiantes.filter((estudiante) => estudiante.id === id)[0]
+	// useEffect(() => {
+	// 	async function getInfo() {
+	// 		const dataEstudiante = await getEstudiante(id)
+	// 		setEstudiante(dataEstudiante)
+	// 		const dataAsistencias = await getAsistencias()
+	// 		setAsistencias(dataAsistencias)
+	// 	}
+	// }, [])
+	const materias = ordenarMaterias(estudiante, asistencias)
 	return (
 		<>
 			<div className='flex flex-col items-center justify-center gap-4 lg:flex-row'>
-				<h2 className='text-4xl '>Historial de Asistencias</h2>
+				<h2 className='text-4xl text-center'>Historial de Asistencias</h2>
 				<button
 					className='mt-auto text-2xl text-blue-500 w-28 hover:underline focus:outline-none'
 					onClick={handleToggleAttendanceView}
@@ -49,9 +46,9 @@ export default function HistorialAsistencias({ id }: Props) {
 			</div>
 			{estudiante && asistencias ? (
 				isSimpleAttendanceView ? (
-					<AsistenciaSimple />
+					<AsistenciaSimple materias={materias} />
 				) : (
-					<AsistenciaDetallada />
+					<AsistenciaDetallada materias={materias} />
 				)
 			) : (
 				<div className='text-center w-full text-xl  italic'>Loading...</div>
