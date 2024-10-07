dfa6c175bf43b0d7b732843bb49b2da65a52b456
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-10-07 11:22:19 -0500

    solucionando problemas con las key y el default value

--- src/modules/attendance/components/student/AsistenciaDetallada.tsx
+++ src/modules/attendance/components/student/AsistenciaDetallada.tsx
@@ -1,84 +1,87 @@
-import type { Estudiante } from '@attendance/constants/apiTypes'
-import type { Materia } from '@attendance/constants/materiasTypes'
-import { Icon } from '@iconify/react'
+import type { Estudiante } from "@attendance/constants/apiTypes";
+import type { Materia } from "@attendance/constants/materiasTypes";
+import { Icon } from "@iconify/react";
 interface Props {
-	materias: Materia[]
+  materias: Materia[];
 }
 export default function AsistenciaDetallada({ materias }: Props) {
-	return (
-		<section className='flex flex-col gap-4 overflow-x-hidden'>
-			<header className='flex justify-center gap-4'>
-				<select className='px-4 py-2 border-2 rounded-lg outline-none border-blue-50 focus:border-blue-300 bg-blue-50'>
-					<option
-						value='0'
-						disabled
-						selected
-					>
-						Curso
-					</option>
-					<option value='1'>Calculo I</option>
-					<option value='2'>Dibujo</option>
-					<option value='3'>Programación</option>
-					<option value='4'>Base De Datos I</option>
-				</select>
-				<select className='px-4 py-2 border-2 rounded-lg outline-none border-blue-50 focus:border-blue-300 bg-blue-50'>
-					<option
-						value='0'
-						disabled
-						selected
-					>
-						Filtrar
-					</option>
-					<option value='1'>Todo</option>
-					<option value='2'>Asistencias</option>
-					<option value='3'>Inasistencias</option>
-				</select>
-			</header>
-			{/* este div es una linea divisora */}
-			<div className='w-full h-px bg-gray-300'></div>
-			{/*className='overflow-y-auto max-h-[1000px]' esto es por si acaso pensamos en hacerlo como una area delimitada*/}
-			<div className='flex flex-col gap-4'>
-				{materias.map((materia) => (
-					<div className='flex flex-col p-4 border-2 rounded-lg overflow-x-auto '>
-						<table className=''>
-							<thead>
-								<tr>
-									<th className='py-4 text-left min-w-72'>Curso</th>
-									<th className='border-l min-w-36'>Horario</th>
-									<th className='border-l min-w-36'>Registro</th>
-									<th className='border-l min-w-36'>Asistencia</th>
-								</tr>
-							</thead>
-							<tbody>
-								{materia.dias.map((dia) => (
-									<tr className='border-t'>
-										<td className='py-6 text-lg'>{materia.nombre}</td>
-										<td className='text-center border-l'>
-											{dia.nombre} <div>{`${dia.inicio.slice(0, -3)} - ${dia.fin.slice(0, -3)}`}</div>
-										</td>
-										<td className='text-center border-l'>
-											{dia.asistencia.fecha} <div>{dia.asistencia.hora.slice(0, -3)}</div>
-										</td>
-										<td className='border-l'>
-											{dia.asistencia.valor ? (
-												<Icon
-													icon='mingcute:check-2-fill'
-													className='mx-auto text-2xl text-green-600'
-												/>
-											) : (
-												<Icon
-													icon='mingcute:close-fill'
-													className='mx-auto text-2xl text-red-600'
-												/>
-											)}
-										</td>
-									</tr>
-								))}
-							</tbody>
-						</table>
-					</div>
-				))}
-			</div>
-		</section>
-	)
+  return (
+    <section className="flex flex-col gap-4 overflow-x-hidden">
+      <header className="flex justify-center gap-4">
+        <select
+          defaultValue="0"
+          className="px-4 py-2 border-2 rounded-lg outline-none border-blue-50 focus:border-blue-300 bg-blue-50"
+        >
+          <option value="0" disabled>
+            Curso
+          </option>
+          <option value="1">Calculo I</option>
+          <option value="2">Dibujo</option>
+          <option value="3">Programación</option>
+          <option value="4">Base De Datos I</option>
+        </select>
+        <select
+          defaultValue="0"
+          className="px-4 py-2 border-2 rounded-lg outline-none border-blue-50 focus:border-blue-300 bg-blue-50"
+        >
+          <option value="0" disabled>
+            Filtrar
+          </option>
+          <option value="1">Todo</option>
+          <option value="2">Asistencias</option>
+          <option value="3">Inasistencias</option>
+        </select>
+      </header>
+      {/* este div es una linea divisora */}
+      <div className="w-full h-px bg-gray-300"></div>
+      {/*className='overflow-y-auto max-h-[1000px]' esto es por si acaso pensamos en hacerlo como una area delimitada*/}
+      <div className="flex flex-col gap-4">
+        {materias.map((materia) => (
+          <div
+            key={materia.id}
+            className="flex flex-col p-4 border-2 rounded-lg overflow-x-auto "
+          >
+            <table className="">
+              <thead>
+                <tr>
+                  <th className="py-4 text-left min-w-72">Curso</th>
+                  <th className="border-l min-w-36">Horario</th>
+                  <th className="border-l min-w-36">Registro</th>
+                  <th className="border-l min-w-36">Asistencia</th>
+                </tr>
+              </thead>
+              <tbody>
+                {materia.dias.map((dia) => (
+                  <tr key={dia.nombre + "" + materia.id} className="border-t">
+                    <td className="py-6 text-lg">{materia.nombre}</td>
+                    <td className="text-center border-l">
+                      {dia.nombre}{" "}
+                      <div>{`${dia.inicio.slice(0, -3)} - ${dia.fin.slice(0, -3)}`}</div>
+                    </td>
+                    <td className="text-center border-l">
+                      {dia.asistencia.fecha}{" "}
+                      <div>{dia.asistencia.hora.slice(0, -3)}</div>
+                    </td>
+                    <td className="border-l">
+                      {dia.asistencia.valor ? (
+                        <Icon
+                          icon="mingcute:check-2-fill"
+                          className="mx-auto text-2xl text-green-600"
+                        />
+                      ) : (
+                        <Icon
+                          icon="mingcute:close-fill"
+                          className="mx-auto text-2xl text-red-600"
+                        />
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
--- src/modules/attendance/components/student/AsistenciaSimple.tsx
+++ src/modules/attendance/components/student/AsistenciaSimple.tsx
@@ -1,78 +1,70 @@
-import type { Estudiante } from '@attendance/constants/apiTypes'
-import { diasSemana } from '@attendance/constants/diasSemana'
-import type { Materia } from '@attendance/constants/materiasTypes'
-import { Icon } from '@iconify/react'
+import type { Estudiante } from "@attendance/constants/apiTypes";
+import { diasSemana } from "@attendance/constants/diasSemana";
+import type { Materia } from "@attendance/constants/materiasTypes";
+import { Icon } from "@iconify/react";
 interface Props {
-	materias: Materia[]
+  materias: Materia[];
 }
 export default function AsistenciaSimple({ materias }: Props) {
-	return (
-		<section className='flex flex-col gap-4 overflow-hidden'>
-			<header className='flex flex-col items-center gap-4'>
-				<div className='flex gap-4'>
-					<button className='focus:outline-none hover:text-blue-500'>
-						<Icon
-							icon='mingcute:left-fill'
-							className='text-3xl'
-						/>
-					</button>
-					<select className='px-4 py-2 border-2 rounded-lg outline-none border-blue-50 focus:border-blue-300 bg-blue-50'>
-						<option value='1'>Semana 1</option>
-						<option value='2'>Semana 2</option>
-						<option value='3'>Semana 3</option>
-						<option value='4'>Semana 4</option>
-					</select>
-					<button className='focus:outline-none hover:text-blue-500'>
-						<Icon
-							icon='mingcute:right-fill'
-							className='text-3xl'
-						/>
-					</button>
-				</div>
-			</header>
-			{/* este div es una linea divisora */}
-			<div className='w-full h-px bg-gray-300'></div>
-			<div className='flex flex-col p-4 border-2 rounded-lg overflow-auto'>
-				<table>
-					<thead>
-						<tr>
-							<th className='py-4 text-left min-w-72'>Curso</th>
-							<th className='border-l min-w-28'>Lunes</th>
-							<th className='border-l min-w-28'>Martes</th>
-							<th className='border-l min-w-28'>Miércoles</th>
-							<th className='border-l min-w-28'>Jueves</th>
-							<th className='border-l min-w-28'>Viernes</th>
-						</tr>
-					</thead>
-					<tbody>
-						{materias.map((materia) => (
-							<tr
-								key={materia.id}
-								className='text-2xl text-left border-t'
-							>
-								<td className='py-6 text-lg '>{materia.nombre}</td>
+  return (
+    <section className="flex flex-col gap-4 overflow-hidden">
+      <header className="flex flex-col items-center gap-4">
+        <div className="flex gap-4">
+          <button className="focus:outline-none hover:text-blue-500">
+            <Icon icon="mingcute:left-fill" className="text-3xl" />
+          </button>
+          <select className="px-4 py-2 border-2 rounded-lg outline-none border-blue-50 focus:border-blue-300 bg-blue-50">
+            <option value="1">Semana 1</option>
+            <option value="2">Semana 2</option>
+            <option value="3">Semana 3</option>
+            <option value="4">Semana 4</option>
+          </select>
+          <button className="focus:outline-none hover:text-blue-500">
+            <Icon icon="mingcute:right-fill" className="text-3xl" />
+          </button>
+        </div>
+      </header>
+      {/* este div es una linea divisora */}
+      <div className="w-full h-px bg-gray-300"></div>
+      <div className="flex flex-col p-4 border-2 rounded-lg overflow-auto">
+        <table>
+          <thead>
+            <tr>
+              <th className="py-4 text-left min-w-72">Curso</th>
+              <th className="border-l min-w-28">Lunes</th>
+              <th className="border-l min-w-28">Martes</th>
+              <th className="border-l min-w-28">Miércoles</th>
+              <th className="border-l min-w-28">Jueves</th>
+              <th className="border-l min-w-28">Viernes</th>
+            </tr>
+          </thead>
+          <tbody>
+            {materias.map((materia) => (
+              <tr key={materia.id} className="text-2xl text-left border-t">
+                <td className="py-6 text-lg ">{materia.nombre}</td>
 
-								{diasSemana.map((dia) => (
-									<td className='border-l'>
-										{materia.dias.some((d) => d.nombre === dia) &&
-											(materia.dias.find((d) => d.nombre === dia)?.asistencia.valor ? (
-												<Icon
-													icon='mingcute:check-2-fill'
-													className='mx-auto text-green-600'
-												/>
-											) : (
-												<Icon
-													icon='mingcute:close-fill'
-													className='mx-auto text-red-600'
-												/>
-											))}
-									</td>
-								))}
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
-		</section>
-	)
+                {diasSemana.map((dia) => (
+                  <td key={dia + "" + materia.id} className="border-l">
+                    {materia.dias.some((d) => d.nombre === dia) &&
+                      (materia.dias.find((d) => d.nombre === dia)?.asistencia
+                        .valor ? (
+                        <Icon
+                          icon="mingcute:check-2-fill"
+                          className="mx-auto text-green-600"
+                        />
+                      ) : (
+                        <Icon
+                          icon="mingcute:close-fill"
+                          className="mx-auto text-red-600"
+                        />
+                      ))}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
 }
