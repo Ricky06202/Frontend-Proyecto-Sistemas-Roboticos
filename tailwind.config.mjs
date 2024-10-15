bf773d56d571719f9dc415ba878b2a88becfdb9b
Azner01 <azneraparicio@hotmail.com>
2024-10-14 21:22:59 -0500

    Beneficios en el Home

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			listStyleType: {
+				none: 'none',
+				disc: 'disc',
+				decimal: 'decimal',
+				square: 'square',
+				roman: 'upper-roman',
+			}
+		},
 	},
 	plugins: [],
 }
