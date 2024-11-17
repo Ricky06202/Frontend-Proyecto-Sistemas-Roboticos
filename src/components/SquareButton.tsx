3a7696f5ae29f0b3b1ed8185da07c519374d3858
reneruiz30 <reneramses30@gmail.com>
2024-11-17 15:57:39 -0600

    Panel de administracion
    Se creo la pagina donde se alojaran todas la funciones del administrador, se tienen los botones que nos redirige a cada seccion disponible.

---
+++ src/components/SquareButton.tsx
@@ -0,0 +1,34 @@
+interface Props {
+	class?: string
+	text: string
+	passThrough?: boolean
+}
+import { AppPages } from '@constants/pages'
+export default function Button({ text, class: className, passThrough }: Props) {
+	return (
+		<>
+			{passThrough ? (
+				<a
+					href={AppPages.Administrator.Attendance.url}
+					className={
+						'relative px-6 py-3 overflow-hidden font-bold text-white bg-blue-700 rounded-lg group text-center' +
+						className
+					}
+				>
+					<span className='absolute top-0 left-0 w-full h-0 transition-all duration-300 ease-in-out bg-black/10 group-hover:h-full group-hover:bottom-0 group-hover:top-auto'></span>
+					<p className='relative z-10 text-center'>{text}</p>
+				</a>
+			) : (
+				<button
+					className={
+						'relative px-6 py-3 overflow-hidden font-bold text-white bg-blue-700 rounded-lg group text-center ' +
+						className
+					}
+				>
+					<span className='absolute top-0 left-0 w-full h-0 transition-all duration-300 ease-in-out bg-black/10 group-hover:h-full group-hover:bottom-0 group-hover:top-auto'></span>
+					<p className='relative z-10 text-center'>{text}</p>
+				</button>
+			)}
+		</>
+	)
+}
