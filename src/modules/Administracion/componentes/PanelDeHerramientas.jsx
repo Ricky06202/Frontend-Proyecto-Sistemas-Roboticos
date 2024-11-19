2ba1b02ec7ca7ea6558d9a7d2aa5673227553a23
Michelle Justavino <michelle.justavino@utp.ac.pa>
2024-11-19 07:18:41 -0500

    se crearon los botones de administrador, y las paginas de crear y editar usuario

--- src/modules/Administracion/componentes/PanelDeHerramientas.jsx
+++ src/modules/Administracion/componentes/PanelDeHerramientas.jsx
@@ -2,7 +2,7 @@
 export default  function PanelDeHerramientas() {
   return (
     <div class="flex flex-col items-center gap-8 dark:text-white ">
-    <h2 class="text-6xl font-bold justify-center">Panel de administracion</h2>
+    <h2 class="text-6xl font-bold justify-center">Panel de administración</h2>
     <p class="text-xl text-center p-8">
         En esta seccion se accedera a todas las funcionalidades de un administrador
     </p>
