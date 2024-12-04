c5f1797d9aa46d879a3418aacdad01307e287188
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-12-03 23:36:59 -0500

    cambiamos el metodo de redireccion

--- src/modules/authentication/logic/authRedirect.ts
+++ src/modules/authentication/logic/authRedirect.ts
@@ -2,7 +2,9 @@ import type { User } from '@authentication/constants/userTypes'
 import { AppPages } from '@constants/pages'
 
 export function authRedirect(user: User) {
-	if (user.groups[0] === 'Estudiante') window.location.href = AppPages.Student(user.id_estudiante).Attendance.url
-	else if (user.groups[0] === 'Profesor') window.location.href = AppPages.Professor(user.id_estudiante).Attendance.url
-	else if (user.groups[0] === 'Admin') window.location.href = AppPages.Administrator.Panel.url
+	// if (user.groups[0] === 'Estudiante') window.location.href = AppPages.Student(user.id_estudiante).Attendance.url
+	// else if (user.groups[0] === 'Profesor') window.location.href = AppPages.Professor(user.id_estudiante).Attendance.url
+	// else if (user.groups[0] === 'Admin') window.location.href = AppPages.Administrator.Panel.url
+	if (user.groups[0] === 'Admin') window.location.href = AppPages.Administrator.Panel.url
+	else window.location.href = AppPages.Student(user.id_estudiante).Attendance.url
 }
